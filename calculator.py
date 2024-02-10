9f37fc2b6009459fff64a6e7f4211c5720e48a43
mx014 <bradleydaniels480@gmail.com>
2024-02-10 09:39:52 -0600

    slight logic changes

--- calculator.py
+++ calculator.py
@@ -8,9 +8,9 @@
 #Write your code below this line 👇
 
 print("Welcome to mx014's Tip Calculator.")
-bill = input("What was the total bill?\n")
-tip_percentage = input("Enter what percentage tip you'd like to give.\n")
-people = input("How many people are you splitting the bill with?\n")
+bill = float(input("What was the total bill?\n$"))
+tip_percentage = int(input("Enter what percentage tip you'd like to give.\n"))
+people = int(input("How many people are you splitting the bill with?\n"))
 
 tip = f"1.{tip_percentage}"
 pay = (float(bill) / int(people)) * float(tip)
